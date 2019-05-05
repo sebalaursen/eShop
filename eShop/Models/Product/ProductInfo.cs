418002a560e8c14cdd2018ddf21351f47a7c3531
sebalaursen <sebalaursen@gmail.com>
2019-05-05 21:04:40 +0300

    Model, view improvements

--- eShop/Models/Product/ProductInfo.cs
+++ eShop/Models/Product/ProductInfo.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 namespace eShop.Models.Product
 {
-    public class ProductCell
+    public class ProductInfo
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Subtitle { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
+        public double Raiting { get; set; }
+        //enum category
+        //views int
         public ProductImage Image { get; set; }
     }
 }
